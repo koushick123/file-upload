ced0795a4cfb61715aa91f28f88483d3c40c1bf0
Dell <skoushicksuri@gmail.com>
2024-09-27 09:11:38 +0530

    Add log4j2.xml and update POM to build WAR file

--- src/main/java/com/media_upload/upload/UploadServiceApplication.java
+++ src/main/java/com/media_upload/upload/UploadServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,8 +13,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.media_upload"})
 @EnableJpaRepositories(basePackages = {"com.media_upload.repository"})
 @EntityScan(basePackages = {"com.media_upload.domain"})
-public class UploadServiceApplication {
+public class UploadServiceApplication extends SpringBootServletInitializer {
 
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {	 
+		 return application.sources(UploadServiceApplication.class);	 
+	 }
+	 
 	public static void main(String[] args) {
 		System.setProperty("aws.java.v1.disableDeprecationAnnouncement", "true");
 		SpringApplication.run(UploadServiceApplication.class, args);
