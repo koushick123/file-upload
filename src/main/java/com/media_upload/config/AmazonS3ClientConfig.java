28c90c6ad9278a1b0f41141e0b25b0ed11e9610d
Dell <skoushicksuri@gmail.com>
2024-10-21 09:40:42 +0530

    Add S3 access via VPC Endpoint read through AWS Secret manager

--- src/main/java/com/media_upload/config/AmazonS3ClientConfig.java
+++ src/main/java/com/media_upload/config/AmazonS3ClientConfig.java
@@ -2,6 +2,7 @@ package com.media_upload.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +11,15 @@ import org.springframework.context.annotation.Profile;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 /*
  * DefaultAWSCredentialsProviderChain will be used for Cloud deployments 
@@ -31,6 +36,12 @@ public class AmazonS3ClientConfig {
 
     @Value("${aws.account-id:}")
     private String aws_account_id;
+    
+    @Value("${s3.bucket.vpc-endpoint.id}")
+    private String s3vpcesecret;
+    
+    @Autowired
+    SecretsManagerClient secretClient;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AmazonS3ClientConfig.class); 
     
@@ -38,9 +49,11 @@ public class AmazonS3ClientConfig {
     @Bean
     AmazonS3 s3ClientCloud() {
     	LOGGER.info("Build credentials for cloud");
-        return AmazonS3ClientBuilder.standard()
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
-                .build();
+    	return AmazonS3ClientBuilder.standard()
+    			.withEndpointConfiguration(
+						new AwsClientBuilder.EndpointConfiguration(
+								getSecretValue(s3vpcesecret), Regions.AP_SOUTH_1.getName()))
+				.build();
     }
 
     @Profile("dev")
@@ -57,4 +70,14 @@ public class AmazonS3ClientConfig {
     	LOGGER.info("Inside getCredentials");
         return new BasicAWSCredentials(aws_access_key,aws_secret_key,aws_account_id);
     }
+    
+    private String getSecretValue(String secretName) {
+		GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse valueResponse = secretClient.getSecretValue(valueRequest);
+        LOGGER.info("Fetch value for secret = {}",secretName);
+        return valueResponse.secretString();
+	}
 }
