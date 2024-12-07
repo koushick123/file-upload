436146786b9e39dd2abf0892542c060160356b19
unknown <skoushicksuri@gmail.com>
2024-12-07 16:50:07 +0530

    Add AWS Secret manager client config

--- src/main/java/com/media_upload/config/AmazonSecretManagerClientConfig.java
+++ src/main/java/com/media_upload/config/AmazonSecretManagerClientConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
@@ -24,7 +27,34 @@ public class AmazonSecretManagerClientConfig {
 	@Value("${secret-manager.vpc-endpoint.dns}")
     private String secretManagerVpce;
 	
+	@Value("${aws.secret-key:}")
+    private String aws_secret_key;
+
+    @Value("${aws.access-key:}")
+    private String aws_access_key;
+
+    @Value("${aws.account-id:}")
+    private String aws_account_id;
+    
+    @Value("${sns.vpc-endpoint.id}")
+    private String snsVpcEndpoint;
+    
 	private static final Logger LOGGER = LoggerFactory.getLogger(AmazonSecretManagerClientConfig.class);
+	
+	 private AWSCredentials getCredentials(){
+	    	LOGGER.info("Inside getCredentials");
+	        return new BasicAWSCredentials(aws_access_key,aws_secret_key,aws_account_id);
+	    }
+	 
+	@Profile("dev")
+	@Bean
+	AWSSecretsManager getSecretClientDev() {
+		LOGGER.info("Create AWS SecretManager Client");
+    	return AWSSecretsManagerClientBuilder.standard()
+    			.withRegion(Regions.AP_SOUTH_1)
+                .withCredentials(new AWSStaticCredentialsProvider(getCredentials()))
+    	.build();
+	}
 	
 	@Profile("cloud")
 	@Bean
